5b41e64a79c14e2ee19883efdc3abfc9c5cccc51
Haroon456 <130932771+Haroon456@users.noreply.github.com>
2024-01-18 21:08:20 +0500

    Add files via upload

---
+++ Lab3/src/Task1/Main.java
@@ -0,0 +1,17 @@
+package Task1;
+
+public class Main extends Thread{
+	public void run() {
+		System.out.println("Task One");
+	}
+	public static void main(String args[]) {
+		Main t1 = new Main();
+		Main t2 = new Main();
+		Main t3 = new Main();
+		
+		t1.start();
+		t2.start();
+		t3.start();
+	}
+
+}
